aa560723a88763caea9aa24ca1d3b651c20f8fcb
liorlr22 <s9077@rehovot.ort.org.il>
2022-11-16 12:31:49 +0200

    update 16/11/22 2

--- front/code.js
+++ front/code.js
@@ -1,5 +1,5 @@
 
 var getYoutubeTitle = require('get-youtube-title')
-        getYoutubeTitle('bpOSxM0rNPM', function (err, title) {
-          console.log(title) // 'SLCHLD - EMOTIONS (feat. RIPELY) (prod. by GILLA)'
+    getYoutubeTitle('bpOSxM0rNPM', function (err, title) {
+      console.log(title) // 'SLCHLD - EMOTIONS (feat. RIPELY) (prod. by GILLA)'
 })    
