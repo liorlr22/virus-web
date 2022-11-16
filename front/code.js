0eae33160a39a624994c8c7eab4601f6b60dd496
liorlr22 <s9077@rehovot.ort.org.il>
2022-11-16 11:26:49 +0200

    update 11/16/22

--- front/code.js
+++ front/code.js
@@ -1,5 +1,5 @@
 
 var getYoutubeTitle = require('get-youtube-title')
-        getYoutubeTitle('ZjM8Wq5pQ2o', function (err, title) {
+        getYoutubeTitle('bpOSxM0rNPM', function (err, title) {
           console.log(title) // 'SLCHLD - EMOTIONS (feat. RIPELY) (prod. by GILLA)'
-    })    
+})    
